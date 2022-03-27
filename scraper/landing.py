8011d7f70305f3ec59623423d9110c5fed78bdac
Naveen Mathew <mathewsinco@gmail.com>
2022-03-27 21:10:02 +0530

    added functions to select announcements section

--- scraper/landing.py
+++ scraper/landing.py
@@ -1,19 +1,13 @@
-import requests
+# Scraping script for landing page of KTU
 from bs4 import BeautifulSoup
 
 
-# Get the soup object of the given url
-def get_url_soup(url):
-    try :
-        response = requests.get(url)
-        soup = BeautifulSoup(response.text, 'html.parser')
-        return soup
-    except requests.ConnectionError:
-        print('[X] Connection Refused; Please check Internet connection')
-    return None
+# Get list of announcements form announcements section
+def get_announcement_section(soup: BeautifulSoup):
+    table = soup.find('ul', {"class": "annuncement"})
+    announcements = table.find_all('li')
+    return announcements
 
-# Get list of rows form the announcements table 
-def get_rows(soup: BeautifulSoup):
-    table = soup.find('table', {"class": "ktu-news"})
-    rows = table.find_all('tr')
-    return rows
+def get_announcement(soup : BeautifulSoup, announcement_no: int):
+    announcement = soup[announcement_no].find('a').text
+    return announcement
