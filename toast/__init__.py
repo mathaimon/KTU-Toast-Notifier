e0e3391e8ed784a4ffe68ed20fdb000ff14828cd
Naveen Mathew <mathewsinco@gmail.com>
2022-04-02 16:53:47 +0530

    added support to open url on click

--- toast/__init__.py
+++ toast/__init__.py
@@ -1,10 +1,22 @@
+import webbrowser
+
 from win10toast_click import ToastNotifier
+
 
 toaster = ToastNotifier()
 
-def show_toast(title: str, description: str):
+def show_toast(title: str, description: str, url: str):
     toaster.show_toast(
         title= title,
         msg= description,
-        duration=3
+        duration=3,
+        callback_on_click= lambda: open_browser(url)
     )
+
+
+def open_browser(url: str):
+    try:
+        webbrowser.open_new(url)
+        print("[+] Opening url in webbrowser")
+    except:
+        print("[X] Failed to open webbrowser")
